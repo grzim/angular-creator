f976972189f23dd58fe7886ecb8729dcf6258bf3
grzim <niebadz@gmail.com>
2014-08-18 22:25:22 +0200

    change files structure

---
+++ js/app.js
@@ -0,0 +1,126 @@
+var mainApp = angular.module("mainApp", ['ngAnimate', 'ngRoute']);
+
+
+mainApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/index.html', {
+            templateUrl: "index.html",
+            controller: "mainCtrl"
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+});
+
+mainApp.service("serviceHints",function(constants){
+    var that = this;
+    this.hints = {};
+    this.show = constants.rootTabName;
+    this.class = "message-disabled";
+    this.toggle = function(name, element){
+        if(angular.isUndefined(that.hints[name])){
+            that.hints[name] = true;
+            that.class = "message-active";
+        }
+        else {
+            that.hints[name] = !that.hints[name];
+            if(that.class=="message-disabled"){
+                that.class="message-active";
+            }
+            else{
+                that.class="message-disabled";
+            }
+        }
+    }
+})
+mainApp.service("sharedProperties",function(){
+    var retriveFromLocalStorage = (function(){
+        var all = [];
+        for(var i = 0;i < localStorage.length; i++){
+            all.push(localStorage.key(i));
+        }
+        return all;
+    }());
+    console.log(this.allNames);
+    var that = this;
+    return {
+        pageName : "",
+        allNames: retriveFromLocalStorage,
+        pageData: { value : ""},
+        getPageData: function(){
+            return this.pageData;
+        },
+        setPageData: function(obj){
+            this.pageData.value=obj;
+            this.pageName = obj.summary.urlName;
+            this.allNames.push(obj.summary.urlName);
+        },
+        loadPageData: function(obj){
+            this.pageData.value=obj;
+            this.pageName = obj.summary.urlName;
+        },
+        getPageName: function(){
+            return this.pageName;
+        },
+        setPageName: function(name){
+            this.pageName = name;
+            this.allNames.push(name);
+        },
+        getAllNames: function(){
+            return this.allNames;
+        }
+    }
+   });
+
+mainApp.constant("constants",{
+    mainTabsNumber : 7,
+    subTabsNumber : 7,
+    maxPictures : 3,
+    maxGallery : 18,
+    startPictures: 1,
+    startGallery: 5,
+    rootTabName: 'main menu',
+    rootTabLevelName: 'tabs'
+})
+
+function r(val){
+    return val = val || 0;
+}
+
+function arrayInterface(obj, arrayName){
+    this[arrayName] = [];
+
+    var array = this[arrayName];
+    this.push = function(){
+        var element = new obj();
+        array.push(element);
+    };
+    this.remove = function(index){
+        array.splice(index, 1);
+    };
+    this.pop = function(){
+        array.pop();
+    };
+    var that = this;
+    this.update = function(number){
+        var length = r(array.length);
+      if(length>number){
+            while(length>number){
+                this.pop();
+                length--;
+            }
+        }
+        if(length<number){
+            while(length<number){
+                this.push();
+                length++;
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ js/controllers.js
@@ -0,0 +1,276 @@
+/**
+ * Created by Grzegorz on 2014-08-10.
+ */
+mainApp.controller("MainCtrl",function($scope, $location){
+    $scope.listeners = {
+        history : []
+        },
+    $scope.section = {
+        questUrl: "",
+        pageUrl: "",
+        quest : true,
+        page : false,
+        toggle : function() {
+
+            this.quest = !this.quest;
+            this.page = !this.page;
+        },
+        goToQuest: function(){
+            this.quest=true;
+            this.page=false;
+        },
+        goToPage: function(){
+            this.quest=false;
+            this.page=true;
+        }
+    }
+})
+
+mainApp.controller("PageCtrl",function($scope, constants, sharedProperties){
+    $scope.navigation = {
+        back : function(){
+            window.history.back();
+        }
+    };
+    $scope.properties = sharedProperties;
+    $scope.pageData = sharedProperties.pageData;
+    $scope.name = "";
+    $scope.$watch(sharedProperties.pageData.value,function(newValue,oldValue){
+        if(angular.isDefined(newValue))
+            $scope.name = newValue.summary.urlName;
+    });
+    $scope.hexToBase64 = function(str) {
+        return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+    };
+    $scope.storage = {
+        load : function(name){
+            console.log(name);
+            var obj = localStorage.getItem(name);
+            obj = angular.fromJson(obj);
+            sharedProperties.loadPageData(obj);
+            console.log(obj);
+        },
+        allNames : sharedProperties.allNames,
+        clearAll : function(){
+            $scope.properties.allNames = [];
+            this.allNames = $scope.properties.allNames;
+        localStorage.clear();
+    }
+    }
+    mariusz = sharedProperties;
+    renata = $scope.pageData;
+    kapusniak = $scope;
+});
+
+mainApp.controller("QuestCtrl",function($scope, $location, $rootScope, constants, sharedProperties){
+     $scope.hints = {
+        mainMenu : false,
+        subtabs: false,
+        additionalContent: false
+    }
+    $scope.setQuestUrl = function(url){
+        var loc = $location.path();
+        $location.path(url+'/create');
+        $scope.section.questUrl = $location.path();
+        $scope.listeners.history.push ( $rootScope.$on('$locationChangeSuccess', function(object, newLocation, previousLocation) {
+            if($location.path() === loc)
+                $scope.summary.urlName = "";
+            if($location.path() === $scope.section.questUrl) {
+                var temp = $location.path().split("/");
+                $scope.summary.urlName = temp[temp.length-2];
+            }
+        }) );
+    }
+    $scope.mainTabs = {
+        maxNumberOfTabs: constants.mainTabsNumber,
+        activeMainTab: "",
+        level: 'tabs',
+        numberOfTabs: 0,
+        name: constants.rootTabName,
+        init : function(){
+            arrayInterface.call(this, Tab, "tab");
+            this.update(1);
+            this.numberOfTabs=1;
+        },
+        selectActiveMainTab : function(level,tab){
+        console.log(this.activeMainTab);
+            if (level === "tabs") this.activeMainTab = tab;
+        }
+    }
+    $scope.summary ={
+        name: "",
+        urlName: "",
+        logo: "",
+        motto: "",
+        colors: new Array(3)
+
+    }
+    $scope.fanpage = {
+        followers : "",
+        likeButton : false,
+        url : ""
+    }
+    $scope.getNumber = function(num){
+        var array = new Array(num);
+        return array;
+    }
+    $scope.selectActiveMainTab = function(level,tab){
+        alert('a');
+        console.log($scope.mainTabs.activeMainTab);
+        $scope.$apply(function() {
+            if (level === "tabs") $scope.mainTabs.activeMainTab = tab;
+        })
+    }
+    $scope.getActiveMainTab = function(){
+        return $scope.mainTabs.activeMainTab;
+    }
+
+    $scope.generate = function() {
+        var pageData = {
+            tabs: $scope.mainTabs,
+            summary: $scope.summary,
+            fanpage: $scope.fanpage
+        }
+        console.log(pageData.tabs);
+        sharedProperties.setPageData(pageData);
+        var localStoreData = pageData;
+        //change this to something smarter, ie underscore
+        for(var i in localStoreData.tabs.tab){
+            console.log(localStoreData.tabs.tab[i]);
+            localStoreData.tabs.tab[i].pictures = {};
+            localStoreData.tabs.tab[i].gallery = {};
+            for(var j in  localStoreData.tabs.tab[i].subtabs) {
+                localStoreData.tabs.tab[i].subtab[j].pictures = {};
+                localStoreData.tabs.tab[i].subtab[j].gallery = {};
+            }
+        }
+
+        localStorage.setItem($scope.summary.urlName, angular.toJson(localStoreData, true));
+        $scope.section.questUrl = $location.path();
+        $location.path('/' +"preview");
+        $scope.section.pageUrl = $location.path();
+        $scope.section.goToPage();
+
+        $scope.listeners.history.push ( $rootScope.$on('$locationChangeSuccess', function(object, newLocation, previousLocation) {
+            if($location.path() === $scope.section.questUrl)
+                $scope.section.goToQuest();
+            if($location.path() === $scope.section.pageUrl)
+                $scope.section.goToPage();
+        }) );
+        console.log(pageData);
+
+    }
+
+    $scope.file = {
+        value: {},
+        compute : function(){
+                var pageData = {
+                    tabs: $scope.mainTabs,
+                    summary: $scope.summary,
+                    fanpage: $scope.fanpage
+                }
+                var json = angular.toJson(pageData, true);
+                console.info(json);
+                var blob = new Blob([json], {type: "application/json"});
+                var url = URL.createObjectURL(blob);
+                this.value = url;
+        }
+    }
+
+    $scope.toggleHint = function(elem){
+        $scope.hints[elem] =  !$scope.hints[elem];
+    },
+    $scope.showHint = function(elem){
+        $scope.hints[elem] =  true;
+    }
+
+    $scope.hideHint = function(elem){
+        $scope.hints[elem] =  false;
+    }
+
+    function Tab(){
+        SubTab.call(this);
+        this.level = "subtabs";
+        arrayInterface.call(this, SubTab,  "tab");
+        this.maxNumberOfTabs = constants.subTabsNumber;
+        this.numberOfTabs = 0;
+        this.init = function(){
+            console.log("init");
+        }
+    }
+
+
+    function SubTab(){
+        this.level = "subtab";
+        this.name;
+        this.mainText;
+        this.pictures= {
+            init : function(){
+                arrayInterface.call(this, Picture,  "list");
+                this.update(constants.startPictures);
+                return constants.startPictures;
+            }
+        };
+        this.video = new Video();
+        this.gallery = {
+            init : function(){
+                arrayInterface.call(this, Picture,  "list");
+                this.update(constants.startGallery);
+                return constants.startGallery;
+            }
+        };
+        this.text;
+        this.contactForm = new Contact();
+    }
+
+    function Media(){
+        this.title;
+        this.description;
+        this.file;
+    }
+
+
+    function Picture(){
+        Media.call(this);
+    }
+
+    function Video(){
+        Media.call(this);
+    }
+
+    function Contact() {
+        this.general;
+        this.phone;
+        this.email;
+        this.twitter;
+        this.youtube;
+        this.facebook;
+        this.xpos;
+        this.ypos;
+    }
+
+});
+
+
+mainApp.controller("tabCtrl", function($scope, constants){
+
+    $scope.constants = constants;
+    $scope.pointer = {
+        name : "main menu",
+        level: "tabs",
+        scope : $scope,
+        tabNumber: ""
+    };
+    $scope.log = function(){
+        console.log($scope);
+    }
+    $scope.tabInit = function(name, level, scope, number){
+        $scope.pointer.name = name || "";
+        $scope.pointer.level = level || "tabs";
+        $scope.pointer.scope = scope ||  $scope.mainTabs;
+        $scope.pointer.tabNumber = number || "";
+        $scope.pointer.scope.init();
+        if(level=="subtabs")console.log(name);
+    };
+});
+
